47e46df6af56d4236ef1e26a4dbc562077a4e55e
ledavto <ser_ega@ukr.net>
2024-01-24 16:49:04 +0200

    connect DB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
-const app = express();
+export const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -20,11 +20,3 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
-
-//Sergii     GeZ3SIUFrS09oG2P
-//npm install mongoose --save
-
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { app } from "./app.js";
 
-const app = express();
+//Sergii     GeZ3SIUFrS09oG2P
+//npm install mongoose --save
 
-app.use(express.json());
-app.use(cors())
+const DB_HOST =
+  "mongodb+srv://Sergii:GeZ3SIUFrS09oG2P@cluster0.vvimffh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    //console.log("Server is running. Use our API on port: 3000");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.use(express.json());
+// app.use(cors());
